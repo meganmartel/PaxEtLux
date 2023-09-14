57507404f0f1649f2dc343713a0af79f90507ebe
Megan Martel <115752040+meganmartel@users.noreply.github.com>
2023-09-13 20:26:00 -0400

    create service worker

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'static-cache-v1';
+
+const FILES_TO_CACHE = [
+    'offline.html',
+];
+
+self.addEventListener('install', (evt) => {
+    console.log('[ServiceWorker] Install');
+
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('[ServiceWorker] Pre-caching offline page');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+    console.log('[ServiceWorker] Activate');
+
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+    console.log('[ServiceWorker] Fetch', evt.request.url);
+
+});
