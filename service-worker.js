adc693b280510113b8cf07ed57cdfd0e35da479b
Megan Martel <115752040+meganmartel@users.noreply.github.com>
2023-09-14 19:37:10 -0400

    debug service worker

--- service-worker.js
+++ service-worker.js
@@ -64,6 +64,19 @@ self.addEventListener('install', (evt) => {
 self.addEventListener('activate', (evt) => {
     console.log('[ServiceWorker] Activate');
 
+    evt.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(
+                keyList.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        console.log('[ServiceWorker] Removing old cache', key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
     self.clients.claim();
 });
 
